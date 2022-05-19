01807d00b3335eb1703f67c0f218e4ef4be054a3
Nctdtman <137032445@qq.com>
2022-05-19 16:07:14 +0800

    feat: 初始化

---
+++ packages/server/src/main.ts
@@ -0,0 +1,29 @@
+import Koa from 'koa'
+import Router from 'koa-router'
+import koaBody from 'koa-body'
+import { PORT } from './constants'
+
+const app = new Koa()
+
+app.use(koaBody())
+
+const router = new Router()
+
+router.get('/api', async (ctx, next) => {
+  ctx.body = { message: 'Hello World' }
+  await next()
+})
+
+router.post('/api/save', async (ctx, next) => {
+  console.log('save:', ctx.request.body)
+  // ...储存到数据库
+  ctx.body = { message: 'Save data successful', receivedData: ctx.request.body }
+  await next()
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
